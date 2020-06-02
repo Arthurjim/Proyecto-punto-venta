e863fdad0e7c45be98f19d15ba657dbe0acee1ad
Arthurjim <arthurjim013@gmail.com>
2020-06-02 00:56:42 -0500

    first

---
+++ src/containers/Ventas.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Ventas = () => <h1>Ventas</h1>
+
+export default Ventas;
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Ventas from './containers/Ventas'
+ReactDOM.render(<Ventas/>, document.getElementById('root'));
